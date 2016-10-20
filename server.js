e7df8dca882ca65c5374f3ddba3b7de34206040e
Reuben Taveras <rtaveras824@hotmail.com>
2016-10-19 20:41:44 -0400

    Added more routes to server file

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', application_controller);
+// app.use('/recipes', recipes_controller);
 
 var sequelize = require("sequelize");
 var models = require("./models");
