8b63dbc30c10113b60d76d1e1792980892eb7a6f
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2016-10-22 14:21:13 -0400

    adding new control file. FML

---
+++ recipebook_controller.js
@@ -0,0 +1,29 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+router.post("/create", function(req, res){
+
+})
+
+router.get("/:userId", function(req, res){
+
+})
+
+router.get("/:userId/:recipeBookId", function(req, res){
+
+})
+
+router.put("/update", function(req, res){
+
+})
+
+router.post("/addRecipe", function(req, res){
+
+})
+
+router.delete("/delete", function(req, res){
+
+})
+
+module.exports = router;
